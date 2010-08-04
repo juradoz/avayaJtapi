fee783b273cdcb10b7fefbbd245757584197cc07
Daniel Jurado <jurado@gmail.com>
2010-08-04 17:43:36 -0300

    Acrescentei na interface

--- src/javax/telephony/callcenter/ACDAddress.java
+++ src/javax/telephony/callcenter/ACDAddress.java
@@ -10,6 +10,12 @@ public abstract interface ACDAddress extends CallCenterAddress
 
   public abstract int getNumberQueued()
     throws MethodNotSupportedException;
+  
+  public abstract int getAgentsLoggedIn()
+  throws MethodNotSupportedException;
+  
+  public abstract int getAgentsAvailable()
+  throws MethodNotSupportedException;
 
   public abstract Call getOldestCallQueued()
     throws MethodNotSupportedException;
