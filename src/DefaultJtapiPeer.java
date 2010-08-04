6517d05e2f14f84c745b2edcc22fb26d07a09c0f
Daniel Jurado <jurado@gmail.com>
2010-08-04 16:04:51 -0300

    Faltavam estes...

---
+++ src/DefaultJtapiPeer.java
@@ -0,0 +1,14 @@
+/*    */ import com.avaya.jtapi.tsapi.TsapiPeer;
+/*    */ 
+/*    */ public class DefaultJtapiPeer extends TsapiPeer
+/*    */ {
+/*    */   public final String getName()
+/*    */   {
+/* 18 */     return super.getClass().getSuperclass().getName();
+/*    */   }
+/*    */ }
+
+/* Location:           C:\Documents and Settings\Daniel Jurado\Meus documentos\My Dropbox\install\Avaya\jtapi-sdk-5.2.2.483\lib\ecsjtapia.jar
+ * Qualified Name:     DefaultJtapiPeer
+ * JD-Core Version:    0.5.4
+ */
---
+++ src/ECSJtapiVersion.java
@@ -0,0 +1,14 @@
+/*    */ import java.io.PrintStream;
+/*    */ 
+/*    */ public class ECSJtapiVersion
+/*    */ {
+/*    */   public static void main(String[] argv)
+/*    */   {
+/* 11 */     System.out.println("Version of Avaya's implementation of Jtapi is 5.2.0.483");
+/*    */   }
+/*    */ }
+
+/* Location:           C:\Documents and Settings\Daniel Jurado\Meus documentos\My Dropbox\install\Avaya\jtapi-sdk-5.2.2.483\lib\ecsjtapia.jar
+ * Qualified Name:     ECSJtapiVersion
+ * JD-Core Version:    0.5.4
+ */
